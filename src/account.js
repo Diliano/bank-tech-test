be3c6417922e8942d9b001ea7e7e6a65a2d955d1
Dilan Patel <patel_dilan@hotmail.com>
2023-04-19 07:43:06 +0100

    Test passing for Account class 'deposit' method - 'it creates a new transaction, adds it to the transaction array and updates the balance'. 'deposit' method implemented in Account class.

--- src/account.js
+++ src/account.js
@@ -1,8 +1,17 @@
+const Transaction = require('./transaction');
+
 class Account {
 
   constructor() {
     this.balance = 0;
     this.transactions = [];
+  }
+
+  deposit(amount) {
+    const date = new Date();
+    const transaction = new Transaction(date, 'credit', amount);
+    this.transactions.push(transaction);
+    this.balance += amount;
   }
 
 }
--- src/tests/account.test.js
+++ src/tests/account.test.js
@@ -1,4 +1,5 @@
 const Account = require('../account');
+const Transaction = require('../transaction');
 
 describe('Account', () => {
 
@@ -7,6 +8,21 @@ describe('Account', () => {
 
     expect(account.transactions).toEqual([]);
     expect(account.balance).toBe(0);
+  });
+
+  describe('deposit', () => {
+    it('creates a new transaction, adds it to the transaction array and updates the balance', () => {
+      const account = new Account;
+      const amount = 100;
+
+      account.deposit(amount);
+
+      expect(account.transactions.length).toBe(1);
+      expect(account.transactions[0]).toBeInstanceOf(Transaction);
+      expect(account.transactions[0].amount).toBe(amount);
+      expect(account.transactions[0].type).toBe('credit');
+      expect(account.balance).toBe(amount);
+    });
   });
 
 });
